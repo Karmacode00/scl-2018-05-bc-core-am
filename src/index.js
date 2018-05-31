90e80a9d2bfd02e49e90cef71c26819a56b35c56
AndreaL <alafertteo@gmail.com>
2018-05-31 17:49:25 -0400

    Botón limpiar funcionando

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 
 window.onload = () => {
     const btn1 = document.getElementById("button-encrypt");
-    //const btn2 = document.getElementById("button-clear");
-    const btn3 = document.getElementById("button-decrypt");
+    const btn2 = document.getElementById("button-decrypt");
+    const btn3 = document.getElementById("button-clear");
 
     btn1.addEventListener("click", () => {
       let str = document.getElementById("text").value;
@@ -12,17 +12,18 @@ window.onload = () => {
       document.getElementById("output").value = result;
     });
     
-    btn3.addEventListener("click", () => {
+    btn2.addEventListener("click", () => {
        let str = document.getElementById("text").value;
         let num = document.getElementById("offset").value;
         const result = window.decipher.decode(num, str);
         document.getElementById("output").value = result;
 
+    });
     
-    
-        //btn2.addEventListener("click", false, () => {
-      //document.getElementById("text").value = "";
-    
-    //})
-});
-};
+    btn3.addEventListener("click", () => {
+      document.getElementById("text").value = "";
+       
+    });
+
+};  
+
