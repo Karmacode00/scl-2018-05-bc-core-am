4860cc535e189078abf6bd1877c4f2372a023c95
AndreaL <alafertteo@gmail.com>
2018-05-31 17:43:31 -0400

    Botones cifrar y descifrar funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,77 @@
 window.cipher = {
-  encode: () => {
+   encode: (num,str) => {
     /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+      num = parseInt(num);
+      let result = "";
+      
+      for (var i = 0; i < str.length; i++){
+        let char = str[i];
+        let charCode = char.charCodeAt(i);
+       
+    
+        
+        if ( char.match(/[A-Z]/i) ){ // Si es letra 
+        
+          let charCode = str.charCodeAt(i);
+    
+          // Uppercase 
+          if ((charCode >= 65) && (charCode <= 90)) {
+            char = String.fromCharCode(((charCode - 65 + num) % 26) + 65);
+          }
+          // Lowercase 
+          else if ((charCode >= 97) && (charCode <= 122)) {
+            char = String.fromCharCode(((charCode - 97 + num) % 26) + 97);
+          }
+          
+        }
+    
+         result += char;
+
+      }
+
+      return result
+    
+     
+
+    }
+
   }
-}
+
+  window.decipher = {
+  decode: (num,str) => {
+
+    let result = "";
+    num = parseInt(num);
+
+    for (var i = 0; i < str.length; i++){
+      let char = str[i];
+      let charCode = char.charCodeAt(i);
+     
+  
+      
+      if ( char.match(/[A-Z]/i) ){ // Si es letra 
+      
+        let charCode = str.charCodeAt(i);
+  
+        // Uppercase 
+        if ((charCode >= 65) && (charCode <= 90)) {
+          char = String.fromCharCode(((charCode - 65 - num) % 26) + 65);
+        }
+        // Lowercase 
+        else if ((charCode >= 97) && (charCode <= 122)) {
+          char = String.fromCharCode(((charCode - 97 - num) % 26) + 97);
+        }
+        
+      }
+  
+       result += char;
+
+    }
+
+    return result
+  
+
+
+
+  }
+  }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 /* Acá va tu código */
 
-function eraseText() {
-    document.getElementById("input").value = "";
-}
+window.onload = () => {
+    const btn1 = document.getElementById("button-encrypt");
+    //const btn2 = document.getElementById("button-clear");
+    const btn3 = document.getElementById("button-decrypt");
+
+    btn1.addEventListener("click", () => {
+      let str = document.getElementById("text").value;
+      let num = document.getElementById("offset").value;
+      const result = window.cipher.encode(num, str);
+      document.getElementById("output").value = result;
+    });
+    
+    btn3.addEventListener("click", () => {
+       let str = document.getElementById("text").value;
+        let num = document.getElementById("offset").value;
+        const result = window.decipher.decode(num, str);
+        document.getElementById("output").value = result;
+
+    
+    
+        //btn2.addEventListener("click", false, () => {
+      //document.getElementById("text").value = "";
+    
+    //})
+});
+};
